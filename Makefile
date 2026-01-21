541d01ecbad7a5bad5bc2a1bc3d9398235acc85e
cheemx5395 <chinmay.mahajan@joshsoftware.com>
2026-01-21 11:39:06 +0530

    initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: server DatabaseInteractive migrationsUp migrationsDown
+
+DB_URI := postgres://fraud_db:fraud_db@localhost:5433/fraud_db?sslmode=disable
+
+server:
+	@go build -o fraud-detection-lite ./cmd && ./fraud-detection-lite
+
+databaseInteractive:
+	@docker exec -it fraud-detection-lite-postgres-1 psql -U fraud_db -d fraud_db
+
+migrationsUp:
+	@goose -dir internal/db/migrations postgres $(DB_URI) up
+
+migrationsDown:
+	@goose -dir internal/db/migrations postgres $(DB_URI) down
---
+++ cmd/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/cheemx5395/fraud-detection-lite/internal/api"
+	"github.com/cheemx5395/fraud-detection-lite/internal/pkg/constants"
+	"github.com/cheemx5395/fraud-detection-lite/internal/repository"
+	"github.com/cheemx5395/fraud-detection-lite/internal/worker"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// setup env
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading environment")
+		return
+	}
+
+	fmt.Println("Starting Server...")
+	defer fmt.Println("Shutting Down Server...")
+
+	// Initialize DB
+	DB, db, err := repository.InitializeDatabase(ctx)
+	if err != nil {
+		log.Printf("Database Error: %v", err)
+		return
+	}
+	fmt.Println("Connected to Database!")
+	defer db.Close()
+
+	// Initialize Redis
+	RD := worker.InitializeRedis()
+	fmt.Println("Connected to Redis")
+	defer RD.Close()
+
+	// Initializing Router
+	router := api.NewRouter(ctx, DB, RD)
+
+	// CORS middleware
+	cors := cors.New(constants.CorsOptions)
+
+	// Setup the server
+	server := &http.Server{
+		Addr:    os.Getenv("PORT"),
+		Handler: cors.Handler(router),
+	}
+
+	go func() {
+		if err = server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Error starting Server: ", err)
+		}
+	}()
+
+	// Graceful shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	signal.Notify(signalChan, syscall.SIGTERM)
+
+	sig := <-signalChan
+	fmt.Println("Received terminate, gracefully shutting down:", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
+	cancel()
+}
